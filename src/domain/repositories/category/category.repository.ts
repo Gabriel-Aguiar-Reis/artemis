548832a5c21e3d83f99b87a8bfdf54334f321952
Gabriel Aguiar <lugafeagre@gmail.com>
2025-11-19 15:15:59 -0300

    feat: update CategoryRepository to use DTOs from validations schema

--- src/domain/repositories/category/category.repository.ts
+++ src/domain/repositories/category/category.repository.ts
@@ -1,12 +1,14 @@
 import { Category } from '@/src/domain/entities/category/category.entity'
-import { AddCategoryDto } from '@/src/domain/repositories/category/dtos/add-category.dto'
-import { UpdateCategoryDto } from '@/src/domain/repositories/category/dtos/update-category.dto'
+import {
+  CategoryInsertDTO,
+  CategoryUpdateDTO,
+} from '@/src/domain/validations/category.schema'
 import { UUID } from 'crypto'
 
 export abstract class CategoryRepository {
   abstract getCategories: () => Promise<Category[]>
-  abstract addCategory: (dto: AddCategoryDto) => Promise<void>
-  abstract updateCategory: (dto: UpdateCategoryDto) => Promise<void>
+  abstract addCategory: (dto: CategoryInsertDTO) => Promise<void>
+  abstract updateCategory: (dto: CategoryUpdateDTO) => Promise<void>
   abstract deleteCategory: (id: UUID) => Promise<void>
   abstract getCategory: (id: UUID) => Promise<Category | null>
   abstract getActiveCategories: () => Promise<Category[]>
