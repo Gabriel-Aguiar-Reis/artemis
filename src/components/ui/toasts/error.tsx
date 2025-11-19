77bf06ea509bf65bee71765712353a75fd8e9c19
Gabriel Aguiar <lugafeagre@gmail.com>
2025-11-19 14:35:31 -0300

    feat: add text2 parameter to error and success toast components for additional messaging

--- src/components/ui/toasts/error.tsx
+++ src/components/ui/toasts/error.tsx
@@ -1,9 +1,9 @@
-import { Alert, AlertTitle } from '@/src/components/ui/alert'
+import { Alert, AlertDescription, AlertTitle } from '@/src/components/ui/alert'
 import { Toast } from '@/src/components/ui/toasts'
 import { AlertCircle } from 'lucide-react-native'
 import { View } from 'react-native'
 
-export const error: Toast = ({ text1, props }) => (
+export const error: Toast = ({ text1, text2, props }) => (
   <View className="w-4/5 bg-red-50 dark:bg-red-900 rounded-md overflow-hidden">
     <Alert
       icon={props.icon ?? AlertCircle}
@@ -13,6 +13,11 @@ export const error: Toast = ({ text1, props }) => (
       <AlertTitle className="text-red-500 dark:text-red-300">
         {text1}
       </AlertTitle>
+      {text2 && (
+        <AlertDescription className="text-red-400 dark:text-red-200 text-xs">
+          {text2}
+        </AlertDescription>
+      )}
     </Alert>
   </View>
 )
--- src/components/ui/toasts/success.tsx
+++ src/components/ui/toasts/success.tsx
@@ -1,9 +1,9 @@
-import { Alert, AlertTitle } from '@/src/components/ui/alert'
+import { Alert, AlertDescription, AlertTitle } from '@/src/components/ui/alert'
 import { Toast } from '@/src/components/ui/toasts'
 import { CircleCheck } from 'lucide-react-native'
 import { View } from 'react-native'
 
-export const success: Toast = ({ text1, props }) => (
+export const success: Toast = ({ text1, text2, props }) => (
   <View className="w-4/5 bg-green-50 dark:bg-green-900 rounded-md overflow-hidden">
     <Alert
       icon={props.icon ?? CircleCheck}
@@ -13,6 +13,11 @@ export const success: Toast = ({ text1, props }) => (
       <AlertTitle className="text-green-500 dark:text-green-300">
         {text1}
       </AlertTitle>
+      {text2 && (
+        <AlertDescription className="text-green-400 dark:text-green-200 text-xs">
+          {text2}
+        </AlertDescription>
+      )}
     </Alert>
   </View>
 )
