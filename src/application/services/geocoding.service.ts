a037ebc3530ef1b6a8ff1bdcca1978cb06131553
Gabriel Aguiar <lugafeagre@gmail.com>
2025-11-24 20:44:59 -0300

    feat: add getAddressByZipCode method to GeocodingService for address retrieval by ZIP code

--- src/application/services/geocoding.service.ts
+++ src/application/services/geocoding.service.ts
@@ -1,4 +1,7 @@
-import { AddressSerializableDTO } from '@/src/domain/entities/customer/value-objects/address.vo'
+import {
+  Address,
+  AddressSerializableDTO,
+} from '@/src/domain/entities/customer/value-objects/address.vo'
 import { Coordinates } from '@/src/domain/entities/customer/value-objects/coordinates.vo'
 
 export class GeocodingService {
@@ -21,6 +24,30 @@ export class GeocodingService {
     return Coordinates.fromDTO({
       latitude: parseFloat(data[0].lat),
       longitude: parseFloat(data[0].lon),
+    })
+  }
+
+  static async getAddressByZipCode(
+    zipCode: string
+  ): Promise<AddressSerializableDTO> {
+    const formattedZip = encodeURIComponent(zipCode)
+    const url = `https://brasilapi.com.br/api/cep/v2/${formattedZip}`
+    const res = await fetch(url, { headers: { 'User-Agent': 'artemis/1.0' } })
+    const data = await res.json()
+    console.log('GeocodingService data:', data)
+
+    if (!data || data.length === 0) {
+      throw new Error('Endereço não encontrado na API de geocodificação.')
+    }
+
+    return Address.fromDTO({
+      streetName: data.street,
+      streetNumber: 0,
+      neighborhood: data.neighborhood,
+      city: data.city,
+      state: data.state,
+      zipCode: data.cep,
+      coordinates: { latitude: 0, longitude: 0 },
     })
   }
 }
