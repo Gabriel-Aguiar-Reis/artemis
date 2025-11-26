f915c93d65bee6c7556e30b23023fc823808cf20
Gabriel Aguiar <lugafeagre@gmail.com>
2025-11-25 21:07:50 -0300

    feat: enhance customer validation schema with detailed error messages and regex patterns

--- src/domain/validations/customer.schema.ts
+++ src/domain/validations/customer.schema.ts
@@ -6,54 +6,191 @@ import {
 } from 'drizzle-zod'
 import z from 'zod'
 
+const ADDRESS_ZIP_CODE_REGEX = /^[0-9]{5}-[0-9]{3}$/
+const ADDRESS_ZIP_CODE_MESSAGE =
+  "CEP inválido. Deve conter 8 dígitos e o formato 'XXXXX-XXX'."
+
+const STORE_NAME_MIN_LENGTH_MESSAGE =
+  'O nome da loja deve ter no mínimo 2 caracteres.'
+const STORE_NAME_MAX_LENGTH_MESSAGE =
+  'O nome da loja deve ter no máximo 100 caracteres.'
+
+const CONTACT_NAME_MIN_LENGTH_MESSAGE =
+  'O nome do contato deve ter no mínimo 2 caracteres.'
+const CONTACT_NAME_MAX_LENGTH_MESSAGE =
+  'O nome do contato deve ter no máximo 100 caracteres.'
+
+const PHONE_NUMBER_REGEX = /^\+\d{1,3}\s\(\d{2}\)\s\d{4,5}-\d{4}$/
+const PHONE_NUMBER_MESSAGE =
+  'O número de telefone deve ser no padrão +XX (XX) XXXXX-XXXX.'
+
+const LANDLINE_NUMBER_REGEX = /^\+\d{1,3}\s\(\d{2}\)\s\d{4}-\d{4}$/
+const LANDLINE_NUMBER_MESSAGE =
+  'O número de telefone fixo deve ser no padrão +XX (XX) XXXX-XXXX.'
+
+const ADDRESS_STREET_NAME_MIN_LENGTH_MESSAGE =
+  'O nome da rua deve ter no mínimo 2 caracteres.'
+const ADDRESS_STREET_NAME_MAX_LENGTH_MESSAGE =
+  'O nome da rua deve ter no máximo 100 caracteres.'
+
+const ADDRESS_STREET_NUMBER_MIN_LENGTH_MESSAGE =
+  'O número da rua deve ter no mínimo 1 caractere.'
+const ADDRESS_STREET_NUMBER_MAX_LENGTH_MESSAGE =
+  'O número da rua deve ter no máximo 10 caracteres.'
+
+const ADDRESS_NEIGHBORHOOD_MIN_LENGTH_MESSAGE =
+  'O nome do bairro deve ter no mínimo 2 caracteres.'
+const ADDRESS_NEIGHBORHOOD_MAX_LENGTH_MESSAGE =
+  'O nome do bairro deve ter no máximo 100 caracteres.'
+
+const ADDRESS_CITY_MIN_LENGTH_MESSAGE =
+  'O nome da cidade deve ter no mínimo 2 caracteres.'
+const ADDRESS_CITY_MAX_LENGTH_MESSAGE =
+  'O nome da cidade deve ter no máximo 100 caracteres.'
+
+const ADDRESS_STATE_MIN_LENGTH_MESSAGE =
+  'O nome do estado deve ter no mínimo 2 caracteres.'
+const ADDRESS_STATE_MAX_LENGTH_MESSAGE =
+  'O nome do estado deve ter no máximo 100 caracteres.'
+
 export const customerSelectSchema = createSelectSchema(customer, {
   id: () => z.uuid('UUID inválido'),
-  storeName: () => z.string().min(2).max(100),
-  contactName: () => z.string().min(2).max(100),
-  phoneNumber: () => z.string().min(10).max(15),
+  storeName: () =>
+    z
+      .string()
+      .min(2, STORE_NAME_MIN_LENGTH_MESSAGE)
+      .max(100, STORE_NAME_MAX_LENGTH_MESSAGE),
+  contactName: () =>
+    z
+      .string()
+      .min(2, CONTACT_NAME_MIN_LENGTH_MESSAGE)
+      .max(100, CONTACT_NAME_MAX_LENGTH_MESSAGE),
+  phoneNumber: () => z.string().regex(PHONE_NUMBER_REGEX, PHONE_NUMBER_MESSAGE),
   phoneIsWhatsApp: () => z.boolean(),
-  landlineNumber: () => z.string().min(10).max(15).nullable(),
+  landlineNumber: () =>
+    z.string().regex(LANDLINE_NUMBER_REGEX, LANDLINE_NUMBER_MESSAGE).nullable(),
   landlineIsWhatsApp: () => z.boolean(),
-  addressStreetName: () => z.string().min(2).max(100),
-  addressStreetNumber: () => z.string().min(1).max(10),
-  addressNeighborhood: () => z.string().min(2).max(100),
-  addressCity: () => z.string().min(2).max(100),
-  addressState: () => z.string().min(2).max(100),
-  addressZipCode: () => z.string().min(5).max(10),
+  addressStreetName: () =>
+    z
+      .string()
+      .min(2, ADDRESS_STREET_NAME_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_STREET_NAME_MAX_LENGTH_MESSAGE),
+  addressStreetNumber: () =>
+    z
+      .string()
+      .min(1, ADDRESS_STREET_NUMBER_MIN_LENGTH_MESSAGE)
+      .max(10, ADDRESS_STREET_NUMBER_MAX_LENGTH_MESSAGE),
+  addressNeighborhood: () =>
+    z
+      .string()
+      .min(2, ADDRESS_NEIGHBORHOOD_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_NEIGHBORHOOD_MAX_LENGTH_MESSAGE),
+  addressCity: () =>
+    z
+      .string()
+      .min(2, ADDRESS_CITY_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_CITY_MAX_LENGTH_MESSAGE),
+  addressState: () =>
+    z
+      .string()
+      .min(2, ADDRESS_STATE_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_STATE_MAX_LENGTH_MESSAGE),
+  addressZipCode: () =>
+    z.string().regex(ADDRESS_ZIP_CODE_REGEX, ADDRESS_ZIP_CODE_MESSAGE),
 })
 
 export type CustomerSelectDTO = z.infer<typeof customerSelectSchema>
 
 export const customerInsertSchema = createInsertSchema(customer, {
-  storeName: () => z.string().min(2).max(100),
-  contactName: () => z.string().min(2).max(100),
-  phoneNumber: () => z.string().min(10).max(15),
+  storeName: () =>
+    z
+      .string()
+      .min(2, STORE_NAME_MIN_LENGTH_MESSAGE)
+      .max(100, STORE_NAME_MAX_LENGTH_MESSAGE),
+  contactName: () =>
+    z
+      .string()
+      .min(2, CONTACT_NAME_MIN_LENGTH_MESSAGE)
+      .max(100, CONTACT_NAME_MAX_LENGTH_MESSAGE),
+  phoneNumber: () => z.string().regex(PHONE_NUMBER_REGEX, PHONE_NUMBER_MESSAGE),
   phoneIsWhatsApp: () => z.boolean(),
-  landlineNumber: () => z.string().min(10).max(15).nullable(),
-  landlineIsWhatsApp: () => z.boolean().nullable(),
-  addressStreetName: () => z.string().min(2).max(100),
-  addressStreetNumber: () => z.string().min(1).max(10),
-  addressNeighborhood: () => z.string().min(2).max(100),
-  addressCity: () => z.string().min(2).max(100),
-  addressState: () => z.string().min(2).max(100),
-  addressZipCode: () => z.string().min(5).max(10),
+  landlineNumber: () =>
+    z.string().regex(LANDLINE_NUMBER_REGEX, LANDLINE_NUMBER_MESSAGE).nullable(),
+  landlineIsWhatsApp: () => z.boolean(),
+  addressStreetName: () =>
+    z
+      .string()
+      .min(2, ADDRESS_STREET_NAME_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_STREET_NAME_MAX_LENGTH_MESSAGE),
+  addressStreetNumber: () =>
+    z
+      .string()
+      .min(1, ADDRESS_STREET_NUMBER_MIN_LENGTH_MESSAGE)
+      .max(10, ADDRESS_STREET_NUMBER_MAX_LENGTH_MESSAGE),
+  addressNeighborhood: () =>
+    z
+      .string()
+      .min(2, ADDRESS_NEIGHBORHOOD_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_NEIGHBORHOOD_MAX_LENGTH_MESSAGE),
+  addressCity: () =>
+    z
+      .string()
+      .min(2, ADDRESS_CITY_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_CITY_MAX_LENGTH_MESSAGE),
+  addressState: () =>
+    z
+      .string()
+      .min(2, ADDRESS_STATE_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_STATE_MAX_LENGTH_MESSAGE),
+  addressZipCode: () =>
+    z.string().regex(ADDRESS_ZIP_CODE_REGEX, ADDRESS_ZIP_CODE_MESSAGE),
 })
 
 export type CustomerInsertDTO = z.infer<typeof customerInsertSchema>
 
 export const customerUpdateSchema = createUpdateSchema(customer, {
-  storeName: () => z.string().min(2).max(100),
-  contactName: () => z.string().min(2).max(100),
-  phoneNumber: () => z.string().min(10).max(15),
+  storeName: () =>
+    z
+      .string()
+      .min(2, STORE_NAME_MIN_LENGTH_MESSAGE)
+      .max(100, STORE_NAME_MAX_LENGTH_MESSAGE),
+  contactName: () =>
+    z
+      .string()
+      .min(2, CONTACT_NAME_MIN_LENGTH_MESSAGE)
+      .max(100, CONTACT_NAME_MAX_LENGTH_MESSAGE),
+  phoneNumber: () => z.string().regex(PHONE_NUMBER_REGEX, PHONE_NUMBER_MESSAGE),
   phoneIsWhatsApp: () => z.boolean(),
-  landlineNumber: () => z.string().min(10).max(15).nullable(),
-  landlineIsWhatsApp: () => z.boolean().nullable(),
-  addressStreetName: () => z.string().min(2).max(100),
-  addressStreetNumber: () => z.string().min(1).max(10),
-  addressNeighborhood: () => z.string().min(2).max(100),
-  addressCity: () => z.string().min(2).max(100),
-  addressState: () => z.string().min(2).max(100),
-  addressZipCode: () => z.string().min(5).max(10),
+  landlineNumber: () =>
+    z.string().regex(LANDLINE_NUMBER_REGEX, LANDLINE_NUMBER_MESSAGE).nullable(),
+  landlineIsWhatsApp: () => z.boolean(),
+  addressStreetName: () =>
+    z
+      .string()
+      .min(2, ADDRESS_STREET_NAME_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_STREET_NAME_MAX_LENGTH_MESSAGE),
+  addressStreetNumber: () =>
+    z
+      .string()
+      .min(1, ADDRESS_STREET_NUMBER_MIN_LENGTH_MESSAGE)
+      .max(10, ADDRESS_STREET_NUMBER_MAX_LENGTH_MESSAGE),
+  addressNeighborhood: () =>
+    z
+      .string()
+      .min(2, ADDRESS_NEIGHBORHOOD_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_NEIGHBORHOOD_MAX_LENGTH_MESSAGE),
+  addressCity: () =>
+    z
+      .string()
+      .min(2, ADDRESS_CITY_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_CITY_MAX_LENGTH_MESSAGE),
+  addressState: () =>
+    z
+      .string()
+      .min(2, ADDRESS_STATE_MIN_LENGTH_MESSAGE)
+      .max(100, ADDRESS_STATE_MAX_LENGTH_MESSAGE),
+  addressZipCode: () =>
+    z.string().regex(ADDRESS_ZIP_CODE_REGEX, ADDRESS_ZIP_CODE_MESSAGE),
 })
 
 export type CustomerUpdateDTO = z.infer<typeof customerUpdateSchema>
