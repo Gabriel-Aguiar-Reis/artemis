fbae786e7ad6428f41d9fde2a4bc5759c8135eac
Gabriel Aguiar <lugafeagre@gmail.com>
2025-11-26 00:29:40 -0300

    feat: add initial database schema for categories, customers, itineraries, payments, products, and work orders

--- src/infra/db/drizzle/0000_glorious_slayback.sql
+++ src/infra/db/drizzle/0000_glorious_slayback.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `category` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`name` text NOT NULL,
 	`is_active` integer DEFAULT true NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `customer` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`store_name` text NOT NULL,
 	`contact_name` text NOT NULL,
 	`phone_number` text NOT NULL,
@@ -16,13 +16,12 @@ CREATE TABLE `customer` (
 	`address_street_number` integer NOT NULL,
 	`address_neighborhood` text NOT NULL,
 	`address_city` text NOT NULL,
-	`address_zip_code` text NOT NULL,
-	`address_latitude` real NOT NULL,
-	`address_longitude` real NOT NULL
+	`address_state` text NOT NULL,
+	`address_zip_code` text NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `itinerary_work_orders` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`itinerary_id` text NOT NULL,
 	`work_order_id` text NOT NULL,
 	`position` integer NOT NULL,
@@ -32,14 +31,14 @@ CREATE TABLE `itinerary_work_orders` (
 );
 --> statement-breakpoint
 CREATE TABLE `itinerary` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`initial_itinerary_date` text NOT NULL,
 	`final_itinerary_date` text NOT NULL,
 	`is_finished` integer DEFAULT false NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `payment_order` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`method` text NOT NULL,
 	`total_value` real NOT NULL,
 	`installments` integer DEFAULT 1 NOT NULL,
@@ -48,7 +47,7 @@ CREATE TABLE `payment_order` (
 );
 --> statement-breakpoint
 CREATE TABLE `product` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`name` text NOT NULL,
 	`category_id` text NOT NULL,
 	`sale_price` real NOT NULL,
@@ -58,7 +57,7 @@ CREATE TABLE `product` (
 );
 --> statement-breakpoint
 CREATE TABLE `work_order_items` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`work_order_id` text NOT NULL,
 	`product_id` text NOT NULL,
 	`quantity` integer NOT NULL,
@@ -68,7 +67,7 @@ CREATE TABLE `work_order_items` (
 );
 --> statement-breakpoint
 CREATE TABLE `work_order_result_items` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`result_id` text NOT NULL,
 	`product_id` text NOT NULL,
 	`quantity` integer NOT NULL,
@@ -80,18 +79,18 @@ CREATE TABLE `work_order_result_items` (
 );
 --> statement-breakpoint
 CREATE TABLE `work_order_result` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`total_value` real NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE `work_order` (
-	`id` text PRIMARY KEY NOT NULL,
+	`id` text PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	`customer_id` text NOT NULL,
-	`created_at` text NOT NULL,
-	`updated_at` text NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP,
 	`scheduled_date` text NOT NULL,
 	`visit_date` text,
-	`payment_order_id` text NOT NULL,
+	`payment_order_id` text,
 	`status` text NOT NULL,
 	`result_id` text,
 	`notes` text,
