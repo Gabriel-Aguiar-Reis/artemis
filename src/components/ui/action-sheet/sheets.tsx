45248be507885644226086334cd7a8a313d0c36e
Gabriel Aguiar <lugafeagre@gmail.com>
2025-11-27 11:59:48 -0300

    feat: add CustomerOptionsSheet component for customizable action options

--- src/components/ui/action-sheet/sheets.tsx
+++ src/components/ui/action-sheet/sheets.tsx
@@ -1,15 +1,23 @@
 import {
+  CustomerOptionsSheet,
+  CustomerOptionsSheetPayload,
+} from '@/src/components/ui/action-sheet/customer-options-sheet'
+import {
   OptionsSheet,
   OptionsSheetPayload,
 } from '@/src/components/ui/action-sheet/options-sheet'
 import { registerSheet, SheetDefinition } from 'react-native-actions-sheet'
 
 registerSheet('options-sheet', OptionsSheet)
+registerSheet('customer-options-sheet', CustomerOptionsSheet)
 
 declare module 'react-native-actions-sheet' {
   interface Sheets {
     'options-sheet': SheetDefinition<{
       payload: OptionsSheetPayload
+    }>
+    'customer-options-sheet': SheetDefinition<{
+      payload: CustomerOptionsSheetPayload
     }>
   }
 }
