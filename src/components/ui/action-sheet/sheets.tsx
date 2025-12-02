06b5ebb623d28ccb87e5dee99cf3271e888747be
Gabriel Aguiar <lugafeagre@gmail.com>
2025-12-01 23:56:32 -0300

    feat: add date picker component and input; update package dependencies

--- src/components/ui/action-sheet/sheets.tsx
+++ src/components/ui/action-sheet/sheets.tsx
@@ -1,15 +1,23 @@
 import {
+  DatePickerSheet,
+  DatePickerSheetPayload,
+} from '@/src/components/ui/action-sheet/date-picker-sheet'
+import {
   OptionsSheet,
   OptionsSheetPayload,
 } from '@/src/components/ui/action-sheet/options-sheet'
 import { registerSheet, SheetDefinition } from 'react-native-actions-sheet'
 
 registerSheet('options-sheet', OptionsSheet)
+registerSheet('date-picker-sheet', DatePickerSheet)
 
 declare module 'react-native-actions-sheet' {
   interface Sheets {
     'options-sheet': SheetDefinition<{
       payload: OptionsSheetPayload
+    }>
+    'date-picker-sheet': SheetDefinition<{
+      payload: DatePickerSheetPayload
     }>
   }
 }
